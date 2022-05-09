8cd1dd500318234348ce8f32e52e3588b9073c67
Jessica Sampaio Aranha dos Santos <sampaioaranha@hotmail.com>
2022-05-09 18:59:41 -0300

    variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,68 @@
+//EXERCICIO DE LEITURA DE CODIGO
+//let a = 10
+//let b = 10
+
+//console.log(b)
+
+//b = 5
+//console.log(a, b)
+
+//Esse imprime os numeros comandados mesmo que estejam em linhas diferentes. (10,10,5)
+
+//let a = 10
+//let b = 20
+//c = a
+//b = c
+//a = b
+
+//console.log(a, b, c)
+//Nesse exercicio todos  valores da variavel ligam a letra com o resultado 10, consequentemente a impressao é sempre 10.   
+//let p = prompt("Quantas horas diarias trabalhadas ?")
+//let t = prompt("Quanto recebe por elas?")
+//alert(`Voce recebe ${t/p} por hora`)
+
+ //EXERCICIO DE ESCRITA DE CODIGO
+//1
+
+let nomeDaPessoa
+let idadeDaPessoa
+console.log (typeof nome, typeof idade)
+//Como não coloquei um valor, a impressão saiu como indefinido, mas se eu tivesse colocado um valor o resultado seria diferente.
+
+
+
+const seuNome = prompt("Qual o seu nome?")
+console.log(seuNome)
+const suaIdade = prompt("Qual a sua idade?")
+console.log(suaIdade)
+//Como eu coloquei um valor a minha variavel, ela imprimiu corretamente o perguntado
+
+ 
+
+ console.log("Olá", "Seu nome é", seuNome, "você tem", suaIdade, "anos")
+
+ let pergunta1 = prompt ("Você quer desenhar comigo?")
+ console.log(pergunta1)
+let pergunta2 = prompt("Você gosta de suco de melancia?")
+console.log (pergunta2)
+let pergunta3 = prompt("Você vai para a escola hoje?")
+console.log (pergunta3)
+ 
+//3
+let a = "10"
+let b = "25"
+let c = null
+a = c
+b = a
+c = b
+console.log ("o novo valor de a é", a)
+console.log("o novo valor de b é", b)
+
+
+
+
+
+
+
+
+
