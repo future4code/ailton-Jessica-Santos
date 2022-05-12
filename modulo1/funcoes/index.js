2f0902714fd5e042b82bf9364ef68361ff94c288
Jessica Sampaio Aranha dos Santos <sampaioaranha@hotmail.com>
2022-05-12 17:18:52 -0300

    aula

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,104 @@
+/*#Exercícios de interpretação de código
+1-EXERCÍCIO */
+
+/*function minhaFuncao(variavel) {
+	return variavel * 5
+}
+
+console.log(minhaFuncao(2))
+console.log(minhaFuncao(10))
+ R- OS VALORES SERÃO MULTIPLICADOS ( 10-50)
+ R- OS VALORES NÃO SERÃO IMPRESSOS*/
+ 
+ /*2-EXERCÍCIO
+ let textoDoUsuario = prompt("Insira um texto");
+
+const outraFuncao = function(texto) {
+	return texto.toLowerCase().includes("cenoura")
+}
+
+const resposta = outraFuncao(textoDoUsuario)
+console.log(resposta)
+R-ELA VAI PEGAR O TEXTO FEITO USUÁRIO E VAI COLOCAR EM LETRA MINUSCULA E VAI DIZER SE NO TEXTO TEM OU NÃO A PALAVRA CENOURA.
+B- i.   Eu gosto de cenoura
+     ii.  CENOURA é bom pra vista
+     iii. Cenouras crescem na terra
+R- i- true
+ii-false
+iii-false
+ 
+
+#Exercícios de escrita de código
+1-A-
+*/
+/*function imprimirMensagem(){
+   console.log ("Meu nome é Jéssica, tenho 24 anos, sou do Rio de Janeiro e amo cozinhar.")
+ 
+} 
+imprimirMensagem()*/
+
+ //B-
+ 
+ /*const nome = 'Laura'
+ const idade = 6
+ const endereço = 'Rio de Janeiro'
+ const profissão = 'Estudante'
+function quemSou (nome, idade, endereço, profissão){
+return `Eu sou a ${nome} tenho ${idade} anos, moro no ${endereço} e sou ${profissão}`
+}
+console.log(`Eu sou a ${nome} tenho ${idade} anos, moro no ${endereço} e sou ${profissão}`)*/
+
+/*2-
+A-
+const primeiroNumero = 25
+const segundoNumero = 13
+function soma(a, b){
+   let soma = 25 + 13
+   return soma
+}
+console.log (soma())*/
+//B-
+/*a= 32
+b=14
+ function maiorouIgual (a, b){
+
+   let maiorouIgual = 32 >= 13
+   return maiorouIgual
+ }
+console.log(maiorouIgual())*/
+ 
+//C- 
+// a=3
+//let total = a % 2 === 0 
+//console.log(total)
+
+/*D-
+ function mensagem (novaMensagem){
+ const mensagemTamanho = novaMensagem.length 
+ return mensagemTamanho
+}
+console.log (mensagem("mensagemTamanho"))
+
+
+function mensagem (novaMensagem){
+ const mensagemMinuscula = novaMensagem.toLowerCase()
+ return mensagemMinuscula
+ }
+ console.log (mensagem("Mensagem Minuscula"))*/
+
+  //3-
+
+ let number1 = +prompt("Insira um número aqui")
+ let number2 = +prompt("Insira outro número")
+ function operacions (number1, number2) {
+ return [number1+number2, number1*number2, number1-number2, number1/number2]
+
+ }
+console.log( number1+number2, number1*number2, number1-number2, number1/number2)
+
+
+
+
+
+
+ 
