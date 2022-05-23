2608df5406bf2d4d194ba523708fb98a425eaa1d
Jessica Sampaio Aranha dos Santos <sampaioaranha@hotmail.com>
2022-05-23 17:09:09 -0300

    exercicio1

---
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -0,0 +1,98 @@
+// ATENÇÃO!!!
+//    -> NÃO COMENTE NENHUMA DAS FUNÇÕES DECLARADAS!!! 
+//    -> NÃO MODIFIQUE OS PARÂMETROS DAS FUNÇÕES!!! ()
+
+
+// EXERCÍCIO 01
+function retornaTamanhoArray(array) {
+  return array.length
+}   
+
+
+// EXERCÍCIO 02
+function retornaArrayInvertido(array) {
+  
+}
+
+// EXERCÍCIO 03
+function retornaArrayOrdenado(array) {
+  
+}
+
+// EXERCÍCIO 04
+function retornaNumerosPares(array) {
+  
+}
+
+// EXERCÍCIO 05
+function retornaNumerosParesElevadosADois(array) {
+ 
+}
+
+// EXERCÍCIO 06
+function retornaMaiorNumero(array) {
+  
+}
+
+// EXERCÍCIO 07
+function retornaObjetoEntreDoisNumeros(num1, num2) {
+
+}
+
+// EXERCÍCIO 08
+function retornaNPrimeirosPares(n) {
+   
+}
+
+// EXERCÍCIO 09
+function classificaTriangulo(ladoA, ladoB, ladoC) {
+
+}
+
+// EXERCÍCIO 10
+function retornaSegundoMaiorESegundoMenor(array) {
+  
+}
+
+// EXERCÍCIO 11
+function retornaChamadaDeFilme(filme) {
+   
+}
+
+// EXERCÍCIO 12
+function retornaPessoaAnonimizada(pessoa) {
+/* return {... pessoa,
+nome:"ANÔNIMO "
+}*/  
+}
+
+// EXERCÍCIO 13A
+function retornaPessoasAutorizadas(pessoas) {
+  /*const pessoasAut = pessoas.filter((autorizadas) =>{
+   return autorizadas.altura>= 1.5 && autorizadas.idade >= 15 && autorizadas.idade < 60
+     
+  }) return pessoasAut*/
+}
+
+// EXERCÍCIO 13B
+function retornaPessoasNaoAutorizadas(pessoas) {
+ /*  const pessoasInv = pessoas.filter ((itens) => {
+return itens.idade <= 14 || itens.idade > 60 || itens.altura <150
+
+   })*/
+}
+
+// EXERCÍCIO 14
+function retornaContasComSaldoAtualizado(contas) {
+
+}
+
+// EXERCÍCIO 15A
+function retornaArrayOrdenadoAlfabeticamente(consultas) {
+//  const ordem = consultas.sort()
+}
+
+// EXERCÍCIO 15B
+function retornaArrayOrdenadoPorData(consultas) {
+   
+}
