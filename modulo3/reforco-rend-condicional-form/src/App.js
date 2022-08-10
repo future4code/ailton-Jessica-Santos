4ec6d8d7432ab4a6bb97bf8a517f7abf171cbe6e
jessica <sampaioaranha@hotmail.com>
2022-08-09 21:25:21 -0300

    reforco-rend-condicional

--- modulo3/reforco-rend-condicional-form/src/App.js
+++ modulo3/reforco-rend-condicional-form/src/App.js
@@ -1,11 +1,37 @@
+import react from 'react'
+import styled from 'styled-components'
 
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Oi
-    </div>
+    <div >
+      <h1>ETAPA 1 - DADOS GERAIS</h1>
+
+<h2>1.Qual o seu nome?</h2>   
+<input
+placeholder={'Seu Nome'}
+value={''}
+onChange={''}
+
+/>
+
+<h2>2.Qual a sua idade?</h2>
+<input
+placeholder={'Sua Idade'}
+value={''}
+onChange={''}
+
+/>
+<h2>3.Qual seu email?</h2>
+<input
+placeholder={'Seu Melhor Email'}
+value={''}
+onChange={''}
+
+/>
+
+
+ </div>
   );
 }
 
