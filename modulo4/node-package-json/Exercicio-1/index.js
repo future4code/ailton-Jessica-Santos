ba2208bc67324edf1a5c58eeebaf12e99f1ab52c
jessica <sampaioaranha@hotmail.com>
2022-08-09 21:20:48 -0300

    node

---
+++ modulo4/node-package-json/Exercicio-1/index.js
@@ -0,0 +1,15 @@
+//Exercício 1
+
+//A) Atraves de um arquivo js, utilizando o console.log e o node no terminal.  
+
+//B)
+
+const minhaIdade = Number(process.argv[3]) + Number(7) 
+console.log("Olá", process.argv[2], "!",  "Você tem", process.argv[3], "anos.", "Em 7 anos você tera",minhaIdade, "anos."  )
+
+
+
+
+
+
+
